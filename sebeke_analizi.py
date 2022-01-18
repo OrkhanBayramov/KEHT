af1a811e9e5c719fe967151c6811842e7851b0f1
OrkhanBayramov <bayorkhan@gmail.com>
2022-01-18 22:47:47 +0400

    Səbəkədə olan cihazlar
    NetMask avtomatik əldə edilir. Şəbəkədə Marşrutlaşdırıcının (routerin) İP ünvanını, Həmçinin Şəbəkədə olan digər qurğuların İP və MAC ünvanı qısa zamanda görüntülənir.

---
+++ sebeke_analizi.py
@@ -0,0 +1,62 @@
+# Azərbaycan dilində intefeysi olan alət.
+# NetMask avtomatik əldə edilir.
+# Şəbəkədə Marşrutlaşdırıcının (routerin) İP ünvanını,
+# Həmçinin Şəbəkədə olan digər qurğuların İP və MAC ünvanı qısa zamanda görüntülənir.
+# Hazırladı Orxan Bayramov.
+# Yazılım dili Python 3
+# 2021-ci il https://www.tty.az
+# !/usr/bin/env python
+
+import subprocess
+import netifaces
+import scapy.all as scapy
+
+
+def printbanner():
+    subprocess.call(["clear"])
+    print("██   ██ ██   ██  █████   ██████   █████  ███    ██    AZERBAIJAN")
+    print("██  ██  ██   ██ ██   ██ ██       ██   ██ ████   ██      ETHICAL ")
+    print("█████   ███████ ███████ ██   ███ ███████ ██ ██  ██      HACKERS ")
+    print("██  ██  ██   ██ ██   ██ ██    ██ ██   ██ ██  ██ ██       TEAM")
+    print("██   ██ ██   ██ ██   ██  ██████  ██   ██ ██   ████  https://kht.az\n")
+
+
+def find_router_ip():  # Şəbəkə routerinin ip ünvanının təyin edilməsi.
+    gw = netifaces.gateways()
+    return gw["default"][netifaces.AF_INET][0]
+
+
+def find_netmask():  # Şəbəkəyə çıxış interfeysinin və şəbəkə maskasının təyini.
+    gw_device = netifaces.gateways()["default"]
+    adapter = gw_device[netifaces.AF_INET][1]
+    gw_netmask = netifaces.ifaddresses(adapter)
+    netmask = gw_netmask[netifaces.AF_INET][0]["netmask"]
+    from netaddr import IPAddress
+    return IPAddress(netmask).netmask_bits()
+
+
+def scan(ip):  # Şəbəkəyə qoşulmuş bütün qurğuların ip və mac ünvanlarının tapılması.
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    clients_list = []
+    for element in answered_list:
+        clients_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(clients_dict)
+    return clients_list
+
+
+def print_result(result_list):
+    print("İP ünvan\t\t  MAC ünvan\n------------------------------------------------------")
+    for client in result_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+printbanner()
+print("-------Şəbəkə analiz olunur!-------")
+ip = find_router_ip() + ("/") + str(find_netmask())
+scan_result = scan(ip)
+print_result(scan_result)
+print("\nRouter IP:", find_router_ip(), "Şəbəkə Maskası:", find_netmask())
